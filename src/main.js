8718021f35c21870e22e009259a34266ebd54c88
paulyarin <pavyarin@yandex.ru>
2021-08-03 22:08:54 +0700

    Отрисованы на страницу блоки рейтинга и карточки фильмов

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ const renderComponent = (container, place, template) => {
 const headerElement = document.querySelector('.header');
 const filmsListElement = document.querySelector('.main');
 
+
 for (let i = 0; i < CARDS_PER_PAGE; i++) {
   renderComponent(filmsListElement, 'beforeend', createFilmCardTemplate());
 }
